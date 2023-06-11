0e91ebec808b1c9c7d3326715fa552a2cdb9d9a3
Hamed Osama <hamedosama19@gmail.com>
2023-06-11 12:02:15 +0300

    add dotenv

--- sanity-config.ts
+++ sanity-config.ts
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env' })
+
 export default {
-  projectId: process.env.SANITY_PROJECT_ID || '1y35ybho',
-  dataset: process.env.SANITY_DATASET  || 'production',
+  projectId: process.env.SANITY_PROJECT_ID ,
+  dataset: process.env.SANITY_DATASET  ,
 }
