faec779c16f2e2372edf713dd5c17148a60b4722
marianWadia <marianwadia55@gmail.com>
2024-08-07 14:55:37 +0300

    added meta tag in the head

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -9,4 +9,14 @@ export const wrapPageElement: GatsbySSR["wrapPageElement"] = ({ element }) => {
     {element}
   </Layout>
   )
-}
+};
+
+export const onRenderBody: GatsbySSR["onRenderBody"] = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <meta
+      name="google-site-verification"
+      content="z-xkRAuhJrHx8NbSs_T3x5OgrXceAA6GYBu7SfR7J64" 
+      key="google-site-verification"
+    />,
+  ]);
+};
