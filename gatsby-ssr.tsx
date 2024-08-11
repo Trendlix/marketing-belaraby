08203763519781ca0899b0383599bd7c286a14d8
MarianIsrael <marianwadia55@gmail.com>
2024-08-11 16:56:51 +0200

    added google AdSense Script tag

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,22 +1,26 @@
-import * as React from "react"
-import type { GatsbySSR } from "gatsby"
-import './src/styles/global.css'
-import Layout from "./src/components/Layout"
+import * as React from "react";
+import type { GatsbySSR } from "gatsby";
+import "./src/styles/global.css";
+import Layout from "./src/components/Layout";
 
 export const wrapPageElement: GatsbySSR["wrapPageElement"] = ({ element }) => {
-  return (
-    <Layout>
-    {element}
-  </Layout>
-  )
+	return <Layout>{element}</Layout>;
 };
 
-export const onRenderBody: GatsbySSR["onRenderBody"] = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <meta
-      name="google-site-verification"
-      content="z-xkRAuhJrHx8NbSs_T3x5OgrXceAA6GYBu7SfR7J64" 
-      key="google-site-verification"
+export const onRenderBody: GatsbySSR["onRenderBody"] = ({
+	setHeadComponents,
+}) => {
+	setHeadComponents([
+    <script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4331921421390242"
+      crossOrigin="anonymous"
+      key="adsense-script"
     />,
-  ]);
+		<meta
+			name="google-site-verification"
+			content="z-xkRAuhJrHx8NbSs_T3x5OgrXceAA6GYBu7SfR7J64"
+			key="google-site-verification"
+		/>,
+	]);
 };
